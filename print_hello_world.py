fb24418c68a02268f3e45c64f9aeb3a4188905aa
Doug Lescarbeau <dlescarbeau@dlescarbeau-mac1.local>
2021-12-23 16:00:52 -0800

    changed it so that the hello world print is called by a different file.

---
+++ print_hello_world.py
@@ -0,0 +1,6 @@
+def print_hello_world():
+	print("Hello, world!")
+
+if __name__ == '__main__':
+	#exicute file as a script
+	print_hello_world()
---
+++ time_for_some_python.py
@@ -0,0 +1,13 @@
+## time to do some pyyython. imma call that other file i just made with this file YO.
+
+import print_hello_world
+
+def service_func():
+	print("executing service function... ")
+
+if __name__ == '__main__':
+	service_func()	
+	print_hello_world.print_hello_world()
+	
+
+
